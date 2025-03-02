036e2d21c01147c5e49a44bae818fb51728fe48f
DanielaMataLugo <150492175+DanielaMataLugo@users.noreply.github.com>
2025-03-01 23:00:00 -0600

    Update P5.java

--- examen_1/P5.java
+++ examen_1/P5.java
@@ -4,10 +4,8 @@ import java.util.Random;
 public class  P5 {
     public static int BusqBin(int[] arr, int num) {
         int izq = 0, der = arr.length - 1;
-
         while (izq <= der) {
             int mitad = izq + (der - izq) / 2; 
-
             if (arr[mitad] == num) {
                 return mitad; 
             } 
@@ -20,7 +18,6 @@ public class  P5 {
         }
         return -1; 
     }
-
     public static void main(String[] args) {
         int[] arr = new int[1000];
         Random rnd = new Random();
@@ -29,11 +26,9 @@ public class  P5 {
         }
         Arrays.sort(arr); 
         int num = arr[rnd.nextInt(1000)]; 
-
         long start = System.nanoTime();
         int res = BusqBin(arr, num);
         long endt = System.nanoTime();
-
         long t = endt - start; 
         System.out.println("Tiempo de ejecución: " + t/1000 + " microsegundos");
     }
